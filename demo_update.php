39d5a6c40d463c67d72e89dd6032c17e83cfbd9a
Baban <205144634+Baban2005@users.noreply.github.com>
2026-04-06 21:42:35 +0800

    Update demo_update.php

--- demo_update.php
+++ demo_update.php
@@ -2,9 +2,7 @@
 // Include your database connection script
 require_once 'softwarelec/core/dbConfig.php';
 
-// ==============================================================================
 // 7. SHOW CODE DEMONSTRATING UPDATING OF RECORD FROM YOUR DATABASE
-// ==============================================================================
 echo "<h3>4. Demonstrating UPDATE</h3>";
 
 $updateQuery = "UPDATE rock_bands SET genre = :newGenre WHERE band_id = :id";
