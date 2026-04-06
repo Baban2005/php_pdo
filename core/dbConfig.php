16d09cc7d1243ccc41c3ec607561a720d6e3f0d0
Baban <205144634+Baban2005@users.noreply.github.com>
2026-04-06 19:46:48 +0800

    Refactor dbConfig.php for consistency and clarity

--- core/dbConfig.php
+++ core/dbConfig.php
@@ -1,28 +1,30 @@
 <?php
 // DBCONFIG.php
 
-$host = '127.0.0.1'; // database host
-$db   = 'senador'; // database name
-$user = 'root'; // database username
-$pass = ''; // database password
+$host = '127.0.0.1'; // database host 
+$db = 'senador'; // database name
+$user = 'root'; // database username 
+$pass = ''; // database password 
 $charset = 'utf8mb4'; // standard encoding
 
 // data source name
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-
+// best practices options for PDO
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // throw exceptions on errors
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // fetch associative arrays by default
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // prevent SQL injection by using actual prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // throw exceptions on errors
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,   // fetch associative arrays by default
+    PDO::ATTR_EMULATE_PREPARES => false, // prevent sql injection by using actual prepared statements
 ];
 
 try {
-    // Create PDO instance
+    // create the PDO instance
     $pdo = new PDO($dsn, $user, $pass, $options);
-    
-    
+
+
+    echo "Connected successfully!";
 } catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+
+    throw new \PDOException($e->getMessage(), (int) $e->getCode());
 }
 ?>
