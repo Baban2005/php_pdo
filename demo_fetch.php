3a3a037565d231a38dbdcc8153b49598c59c3a18
Baban <205144634+Baban2005@users.noreply.github.com>
2026-04-06 21:41:46 +0800

    Clean up comments and improve fetch demonstration
    Removed comments and updated the echo statement for clarity.

--- demo_fetch.php
+++ demo_fetch.php
@@ -2,9 +2,7 @@
 // Include your database connection script
 require_once 'softwarelec/core/dbConfig.php';
 
-// ==============================================================================
 // 4. SHOW CODE DEMONSTRATING HOW FETCH() IS USED. USE PRINT_R(). WITH “<pre>” TAG IN BETWEEN.
-// ==============================================================================
 echo "<h3>2. Demonstrating fetch()</h3>";
 
 // Fetching just a single record 
